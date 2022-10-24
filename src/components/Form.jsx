d56fa94670c411c454deaaecb4357aba80b40887
ilyadyxah <ilyadyxah@gmail.com>
2022-10-24 14:29:27 +0300

    lesson 3

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,13 @@
 import style from './Form.module.css'
-import {useState} from "react";
+import {useRef, useState} from "react";
+import Button from '@mui/material/Button';
+import {TextField} from "@mui/material";
 
 export const Form = ({changeMessages}) => {
     const [text, setText] = useState(null)
     const [name, setName] = useState(null)
+    const inputNameRef = useRef(null)
+    const inputTextRef = useRef(null)
 
     const changeText = (ev) => {
         setText(ev.target.value)
@@ -13,11 +17,22 @@ export const Form = ({changeMessages}) => {
         setName(ev.target.value)
     }
 
+    const sendMessage = () => {
+        changeMessages({name, text})
+        clearData()
+    }
+
+    const clearData = () => {
+        inputNameRef.current.value = null
+        inputTextRef.current.value = null
+        inputNameRef.current.focus()
+    }
+
     return <>
-            <div>
-                <input type="text" name='name' onChange={changeName}/>
-                <input type="text" name='text' onChange={changeText}/>
-                <button onClick={() => {changeMessages({name, text})}}>Отправить</button>
+            <div className={style.textForm}>
+                <TextField inputRef={inputNameRef} autoFocus={true} className={style.textInput} id="outlined-basic" name='name' onChange={changeName} label="Имя" variant="outlined" />
+                <TextField inputRef={inputTextRef} className={style.textInput} id="outlined-basic" name='name' onChange={changeText} label="Текст" variant="outlined" />
+                <Button onClick={sendMessage} variant="contained">Отправить</Button>
             </div>
     </>
 }
